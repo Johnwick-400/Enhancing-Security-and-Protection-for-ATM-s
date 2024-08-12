f8307c8caed26b7ab10728cbe2aa420a307b7394
Pavantej Veesam <98897594+Johnwick-400@users.noreply.github.com>
2024-08-12 15:01:29 +0530

    Create esp32_cam.c

---
+++ esp32_cam.c
@@ -0,0 +1,176 @@
+#include "esp_camera.h"
+#include "SPI.h"
+#include "driver/rtc_io.h"
+#include <ESP_Mail_Client.h>
+#include <FS.h>
+#include <WiFi.h>
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "fruitu";
+const char* password = "12345678";
+
+// To send Email using Gmail use port 465 (SSL) and SMTP Server smtp.gmail.com
+// You need to create an email app password
+#define emailSenderAccount    "cameramodule95@gmail.com"
+#define emailSenderPassword   "yqmr aqvu pmyo osbn"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465
+#define emailSubject          "ESP32-CAM Photo Captured"
+#define emailRecipient        "chinturejeti123@gmail.com"
+
+#define CAMERA_MODEL_AI_THINKER
+
+// Function declarations
+void capturePhotoSaveLittleFS();
+void sendPhoto();
+void processCommand(const String& command);
+
+// Photo File Name to save in LittleFS
+#define FILE_PHOTO "photo.jpg"
+#define FILE_PHOTO_PATH "/photo.jpg"
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  
+  Serial.begin(115200);
+  Serial.println();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  // Print ESP32 Local IP Address
+  Serial.print("IP Address: http://");
+  Serial.println(WiFi.localIP());
+
+  // Init filesystem
+  ESP_MAIL_DEFAULT_FLASH_FS.begin();
+   
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void loop() {
+  // Check if data is available on serial port
+  if (Serial.available() > 0) {
+    // Read command from serial port
+    String command = Serial.readStringUntil('\n');
+    // Process the received command
+    processCommand(command);
+  }
+}
+
+// Process received command
+void processCommand(const String& command) {
+  // If command is to capture and send photo
+  if (command == "CAPTURE_AND_SEND") {
+    capturePhotoSaveLittleFS();
+    sendPhoto();
+  }
+}
+
+// Capture Photo and Save it to LittleFS
+void capturePhotoSaveLittleFS() {
+  // Dispose first pictures because of bad quality
+  camera_fb_t* fb = NULL;
+  // Skip first 3 frames (increase/decrease number as needed).
+  for (int i = 0; i < 3; i++) {
+    fb = esp_camera_fb_get();
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
+    
+  // Take a new photo
+  fb = NULL;  
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }  
+
+  // Photo file name
+  Serial.printf("Picture file name: %s\n", FILE_PHOTO_PATH);
+  File file = LittleFS.open(FILE_PHOTO_PATH, FILE_WRITE);
+
+  // Insert the data in the photo file
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+  }
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.print("The picture has been saved in ");
+    Serial.print(FILE_PHOTO_PATH);
+    Serial.print(" - Size: ");
+    Serial.print(fb->len);
+    Serial.println(" bytes");
+  }
+  // Close the file
+  file.close();
+  esp_camera_fb_return(fb);
+}
+
+// Send Photo via Email
+void sendPhoto() {
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  Session_Config config;
+  
+  /*Set the NTP config time
+  For times east of the Prime Meridian use 0-12
+  For times west of the Prime Meridian add 12 to the offset.
+  Ex. American/Denver GMT would be -6. 6 + 12 = 18
+  See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
+  */
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = 0
